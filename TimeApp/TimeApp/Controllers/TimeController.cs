dcd9ccccbc8e573a196083abd88efd39836f1ca2
pavvlik777 <pavvlik77727@gmail.com>
2023-12-27 00:22:08 +0300

    init

---
+++ TimeApp/TimeApp/Controllers/TimeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimeApp.Controllers
+{
+    [ApiController]
+    [Route("api/time")]
+    public sealed class TimeController : ControllerBase
+    {
+        private static readonly IReadOnlyCollection<TimeData> Timezones;
+
+
+        static TimeController()
+        {
+            Timezones = new List<TimeData>()
+            {
+                new ()
+                {
+                    ZoneId = "minsk",
+                    DisplayName = "Minsk",
+                    UtcOffsetMinutes = 3 * 60,
+                },
+                new ()
+                {
+                    ZoneId = "new_york",
+                    DisplayName = "New York",
+                    UtcOffsetMinutes = -5 * 60,
+                },
+                new ()
+                {
+                    ZoneId = "warsaw",
+                    DisplayName = "warsaw",
+                    UtcOffsetMinutes = 1 * 60,
+                },
+                new ()
+                {
+                    ZoneId = "novosibirsk",
+                    DisplayName = "Novosibirsk",
+                    UtcOffsetMinutes = 7 * 60,
+                },
+            };
+        }
+
+
+        [HttpGet]
+        public ActionResult<IReadOnlyCollection<TimeData>> Get()
+        {
+            return Ok(Timezones);
+        }
+
+        [HttpPost("diff/{first}/{second}")]
+        public IActionResult Diff(string first, string second)
+        {
+            var firstTimeZone = Timezones.SingleOrDefault(z => z.ZoneId == first);
+            if (firstTimeZone == null)
+            {
+                return BadRequest();
+            }
+
+            var secondTimeZone = Timezones.SingleOrDefault(z => z.ZoneId == second);
+            if (secondTimeZone == null)
+            {
+                return BadRequest();
+            }
+
+            //TODO azure functions
+
+            return null;
+        }
+
+
+
+        public sealed class TimeData
+        {
+            public string ZoneId { get; set; }
+
+            public string DisplayName { get; set; }
+
+            public int UtcOffsetMinutes { get; set; }
+        }
+    }
+}
